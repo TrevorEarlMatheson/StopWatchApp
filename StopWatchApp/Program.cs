b3c2720f5e5ea446aa02762117197f5ae8c1d12b
trevor.matheson <trevor.matheson@CORP-L0130>
2021-09-03 07:30:01 -0600

    Add project files.

---
+++ StopWatchApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace StopWatchApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StopWatch stopwatch = new StopWatch();
+            while (true)
+            {
+
+                if(stopwatch.StartTime == DateTime.MinValue)
+                {
+                    stopwatch.StartPrompt();
+                }
+                else
+                {
+                    stopwatch.RunningPrompt();
+                }
+
+                var keyPressed = Console.ReadKey().Key;
+
+                if(keyPressed == ConsoleKey.Enter && stopwatch.StartTime == DateTime.MinValue)
+                {
+                    Console.Clear();
+                    stopwatch.StartTime = DateTime.Now;
+                }
+                else if(keyPressed == ConsoleKey.Enter && stopwatch.StartTime != DateTime.MinValue)
+                {
+                    stopwatch.Duration();
+                }
+                else if(keyPressed == ConsoleKey.Escape)
+                {
+                    break;
+                }
+                else
+                {
+                    stopwatch.InvalidKeyPrompt();
+                }
+            }
+        }
+    }
+}
---
+++ StopWatchApp/StopWatch.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StopWatchApp
+{
+    class StopWatch
+    {
+        private DateTime _startTime;
+        private readonly string  _startPrompt;
+        private readonly string _runningPrompt;
+        private readonly string _invalidPrompt;
+
+        public StopWatch()
+        {
+            _startPrompt = "\nEnter: Start/Stop Timer\nEscape: Quit Program";
+            _runningPrompt = "\nStopwatch Running...";
+            _invalidPrompt = "\nInvalid key pressed.";
+            _startTime = DateTime.MinValue;
+        }
+
+        public DateTime StartTime 
+        {
+            set { _startTime = value; }
+            get { return _startTime; }
+        }
+
+        public void StartPrompt()
+        {
+            Console.WriteLine(_startPrompt);
+        }
+
+        public void InvalidKeyPrompt()
+        {
+            Console.Clear();
+            Console.WriteLine(_invalidPrompt);
+        }
+
+        public void RunningPrompt()
+        {
+            Console.WriteLine(_runningPrompt);
+        }
+
+        public void Duration()
+        {
+            Console.Clear();
+            Console.WriteLine("Elapsed Time: " + (DateTime.Now - _startTime));
+            ResetWatch();
+        }
+
+        private void ResetWatch()
+        {
+            _startTime = DateTime.MinValue;
+        }
+    }
+}
